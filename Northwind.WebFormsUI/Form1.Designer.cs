fbd7e9fff4206c3548ec38fb90a9cab610068637
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2018-12-20 11:05:50 +0300

    Filtreli arayüz düzenlendi

--- Northwind.WebFormsUI/Form1.Designer.cs
+++ Northwind.WebFormsUI/Form1.Designer.cs
@@ -29,35 +29,81 @@
         private void InitializeComponent()
         {
             this.dgvProduct = new System.Windows.Forms.DataGridView();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvProduct
             // 
             this.dgvProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProduct.Location = new System.Drawing.Point(12, 168);
+            this.dgvProduct.Location = new System.Drawing.Point(12, 99);
             this.dgvProduct.Name = "dgvProduct";
-            this.dgvProduct.Size = new System.Drawing.Size(776, 260);
+            this.dgvProduct.Size = new System.Drawing.Size(730, 260);
             this.dgvProduct.TabIndex = 0;
             this.dgvProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProduct_CellContentClick);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(23, 29);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(52, 13);
+            this.lblCategory.TabIndex = 1;
+            this.lblCategory.Text = "Category:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(78, 26);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(262, 21);
+            this.cmbCategory.TabIndex = 2;
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(23, 63);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(78, 13);
+            this.lblProductName.TabIndex = 1;
+            this.lblProductName.Text = "Product Name:";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(107, 60);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(233, 20);
+            this.txtProductName.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(759, 378);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblCategory);
             this.Controls.Add(this.dgvProduct);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProduct)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvProduct;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.TextBox txtProductName;
     }
 }
 
